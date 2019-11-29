3d374831065ee70ead297bcb3fb568db67c150fe
dlckdtn62 <50452621+dlckdtn62@users.noreply.github.com>
2019-11-29 14:08:16 +0900

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,30 @@
+import gym
+import torch
+from actor_critic.actor_critic import ActorCritic
+from actor_critic.actions import get_action_space, get_actions
+from actor_critic.environment_wrapper import EnvironmentWrapper
+
+
+def actor_critic_inference(params, path):
+    model = ActorCritic(params.stack_size, get_action_space())
+    model.load_state_dict(torch.load(path))
+    model.eval()
+
+    env = gym.make('CarRacing-v0')
+    env_wrapper = EnvironmentWrapper(env, params.stack_size)
+
+    state = env_wrapper.reset()
+    state = torch.Tensor([state])
+    done = False
+    total_score = 0
+    while not done:
+        probs, _, _ = model(state)
+        action = get_actions(probs)
+        print(action, path)
+        state, reward, done = env_wrapper.step(action[0])
+        state = torch.Tensor([state])
+        total_score += reward
+        env_wrapper.render()
+    env_wrapper.close()
+    env.close()
+    return total_score
